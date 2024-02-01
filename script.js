24ff19b60e4a19cc11e82cd6c5665f95e6e3b844
Marco <marcotenaglia@gmail.com>
2024-02-01 13:23:05 -0300

    correccion de archivo js

--- script.js
+++ script.js
@@ -1,71 +1,83 @@
 const text = document.getElementById("text");
-
 const addButton = document.getElementById("add-btn");
-
 const clearButton = document.getElementById("clear-btn");
+const uList = document.getElementById("u-list");
+const arr = [];
 
-const uList = document.getElementById("u-list")
+function createListItem(inputVal) {
+    const listItemElement = document.createElement("li");
+    listItemElement.classList.add("li-style");
+    
+    const itemText = createTextElement(inputVal);
+    const buttonContainer = createButtonContainer();
+    
+    listItemElement.appendChild(itemText);
+    listItemElement.appendChild(buttonContainer);
+    
+    return listItemElement;
+}
 
-const arr = []
+function createTextElement(inputVal) {
+    const itemText = document.createElement("div");
+    itemText.classList.add("text");
+    itemText.innerText = inputVal;
+    return itemText;
+}
+
+function createButtonContainer() {
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("btn-container");
+
+    const buttonElement = createButton("✔", "btn-hecho");
+    const deleteButtonElement = createButton("🗑", "btn-x");
+
+    buttonContainer.appendChild(buttonElement);
+    buttonContainer.appendChild(deleteButtonElement);
+
+    return buttonContainer;
+}
+
+function createButton(text, className) {
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = text;
+    buttonElement.classList.add(className);
+    return buttonElement;
+}
+
+function addListEvents(listItemElement, itemText) {
+    const buttonElement = listItemElement.querySelector(".btn-hecho");
+    const deleteButtonElement = listItemElement.querySelector(".btn-x");
+
+    buttonElement.addEventListener("click", () => {
+        itemText.classList.toggle("strike");
+    });
+
+    deleteButtonElement.addEventListener("click", () => {
+        listItemElement.remove();
+    });
+}
 
 function toDoList() {
-    const inputVal = text.value
+    const inputVal = text.value;
 
     if (inputVal.length) {
-        
-        const listItemElement = document.createElement("li");
-        listItemElement.classList.add("li-style")
+        const listItemElement = createListItem(inputVal);
+        const itemText = listItemElement.querySelector(".text");
+
+        addListEvents(listItemElement, itemText);
+
         uList.appendChild(listItemElement);
-
-        const buttonContainer = document.createElement("div")
-        buttonContainer.classList.add("btn-container")
-        const buttonElement = document.createElement("button");        
-        const deleteButtonElement = document.createElement("button"); 
-
-        const itemText = document.createElement("div")
-        itemText.classList.add("text")        
-        listItemElement.appendChild(itemText);
-        itemText.innerText = inputVal;
-        
-
-
-        buttonElement.innerText = "✔" 
-        buttonElement.classList.add("btn-hecho")       
-        deleteButtonElement.innerText = "🗑"
-        deleteButtonElement.classList.add("btn-x")     
-
-        
-        listItemElement.appendChild(buttonContainer);
-        
-        buttonContainer.appendChild(buttonElement);
-
-        buttonContainer.appendChild(deleteButtonElement);
-        
         text.value = "";
-        
-        buttonElement.addEventListener("click", () => {
-            itemText.classList.toggle("strike");
-        })
-        
-        deleteButtonElement.addEventListener("click", () => {
-            listItemElement.remove();
-
-        })
-        arr.push(listItemElement)
-
-    }
-    else {
+        arr.push(listItemElement);
+    } else {
         return false;
     }
-
-
 }
 
 addButton.addEventListener("click", function (e) {
-    toDoList()
-    text.focus()
-})
-
+    toDoList();
+    text.focus();
+});
 
 text.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
@@ -74,9 +86,7 @@ text.addEventListener('keypress', function (e) {
 });
 
 clearButton.addEventListener("click", () => {
-    uList.replaceChildren()
-    text.focus()
+    uList.replaceChildren();
+    text.focus();
     text.value = "";
 });
-
-
